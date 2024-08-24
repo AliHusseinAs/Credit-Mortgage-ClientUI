c99233f7307be2c537b3e36b95a0b9cb5d077cb3
Ali Hussein <aa1355493@gmail.com>
2024-08-24 21:33:13 +0300

    change the userUI HTML and user.css made new UI, added nav bar and created the pages for all nav bars

--- clientSide/JS files/userUI.js
+++ clientSide/JS files/userUI.js
@@ -1,28 +1,40 @@
-// CALCULATE Button configurations within the calculator 
 
-function totalMonthly(amount, down, interest, period){
+function totalMonthly(amount, down, interest, period) {
     const lend = amount - down;
     const totalInterest = lend * interest * period;
-    const total = (lend + totalInterest) / (period*12);
+    const total = (lend + totalInterest) / (period * 12);
     return total;
 }
-function calculate(){
-    const amount = parseFloat(document.getElementById('amountField').value);
-    const down = parseFloat(document.getElementById('downField').value);
-    const interest = parseFloat(document.getElementById('interest').value) / 100;
-    const period = parseInt(document.getElementById('period').value);
+
+
+function calculate() {
+
+    const form = document.getElementById('calculator-form');
+
+
+    const formData = new FormData(form);
+
+
+    const amount = parseFloat(formData.get('amount'));
+    const down = parseFloat(formData.get('down-payment'));
+    const interest = parseFloat(formData.get('interest-rate')) / 100;
+    const period = parseInt(formData.get('period'));
+
     let result = 0;
+
 
     if (isNaN(amount) || isNaN(down) || isNaN(interest) || isNaN(period)) {
         document.getElementById('res').innerHTML = "Please enter valid numbers";
         return;
     }
+
+
     switch (period) {
         case 5:
             result = totalMonthly(amount, down, interest, 5);
             break;
         case 10:
-            result =  totalMonthly(amount, down, interest, 10);
+            result = totalMonthly(amount, down, interest, 10);
             break;
         case 15:
             result = totalMonthly(amount, down, interest, 15);
@@ -31,15 +43,18 @@ function calculate(){
             result = totalMonthly(amount, down, interest, 30);
             break;
         default:
-            result =  "Please select a valid period"
+            result = "Please select a valid period";
             document.getElementById('res').innerHTML = result;
             return;
     }
-    const text = "Your monthly is " + result.toFixed(2);
+
+
+    const text = "Your monthly payment is " + result.toFixed(2);
     document.getElementById('resText').innerHTML = text;
-    console.log("it did work");
-    const element = document.getElementById('applyAfterTextBTN');
-    element.classList.remove('hidden');
-    element.classList.add('applyAfterCalc');
+
+
+    // const element = document.getElementById('applyAfterTextBTN');
+    // element.classList.remove('hidden');
+    // element.classList.add('applyAfterCalc');
 }
 
